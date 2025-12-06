f34b1552a430be225a225fd24aee8a03f29fd387
Hhvost <greench9206@mail.ru>
2025-12-06 15:50:40 +0400

    ui & colours

--- settings.py
+++ settings.py
@@ -12,6 +12,11 @@ BLACK = (0, 0, 0)
 GRAY = (200, 200, 200)
 DARK_BLUE = (30, 50, 100)
 BG_COLOR = (240, 240, 245)
+LIGHT_GRAY = (230, 230, 230)
+GREEN = (50, 200, 50)
+RED = (200, 50, 50)
+BLUE = (50, 100, 200)
+
 
 # Шрифты
 FONT_NAME = 'arial'
